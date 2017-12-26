aef2930bb28b75fe58abd835f60febac1fc9cbaa
Jamie Pilgrim <hello@jamiepilgrim.com>
2017-12-25 17:28:34 -0800

    added frenchify method to HelloWorld

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const HelloWorld = props => {
-  return (<div className="HelloWorld"> Hello {props.name}! </div> )
-};
+class HelloWorld extends Component {
+  constructor(props){
+    super(props);
+    this.state = {greeting: 'Hello'};
+    this.frenchify = this.frenchify.bind(this);
+  }//constructor enabled
+  render () {
+    return (
+      <div className="HelloWorld">
+        <h2>{this.state.greeting} {this.props.name}!</h2>
+        <button onClick={this.frenchify}> Frenchify! </button>
+      </div>
+    );
+  } //render end
+  frenchify() {
+    this.setState({ greeting: 'Bonjour' });
+  }
+
+};//end HelloWorld class
 
 export default HelloWorld;
