5287295aa8bcc0021dca479e308ee9bad6619465
Jamie Pilgrim <hello@jamiepilgrim.com>
2017-12-25 20:06:12 -0800

    add HelloWorldList render

--- src/HelloWorldList.js
+++ src/HelloWorldList.js
@@ -2,3 +2,18 @@ import React, { Component } from 'react';
 import './HelloWorldList.css';
 
 import HelloWorld from './HelloWorld';
+
+
+class HelloWorldList extends Component {
+  render() {
+    return (
+      <div className="HelloWorldList">
+        <HelloWorld name="Amper"/>
+        <HelloWorld name="Octo"/>
+      </div>
+    )
+  }
+}
+
+
+export default HelloWorldList;
