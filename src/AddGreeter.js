63f54084e6744f87b486a30070dfdd546767eda8
Jamie Pilgrim <hello@jamiepilgrim.com>
2017-12-25 22:05:12 -0800

    add constructor and render methods to AddGreeter

--- src/AddGreeter.js
+++ src/AddGreeter.js
@@ -1,5 +1,21 @@
 import React, { Component } from 'react';
 import './AddGreeter.css';
 
+class AddGreeter extends Component {
+  constructor(props){
+    super(props);
+    this.state = {greetingName: ''};
+    this.handleUpdate = this.handleUpdate.bind(this);
+  }
+  render(){
+    return (
+      <div className="AddGreeter">
+        <input type="text" onClick={this.handleUpdate()}/>
+        &nbsp;&nbsp;
+        <button> Add </button>
+      </div>
+    );
+  }
+}
 
 export default AddGreeter;
