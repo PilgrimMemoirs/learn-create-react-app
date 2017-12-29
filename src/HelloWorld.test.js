36d586b5519bf355b6685b0f093edd8726972588
Jamie Pilgrim <hello@jamiepilgrim.com>
2017-12-28 21:10:22 -0800

    add describe HelloWorld block to HelloWorld.test.js

--- src/HelloWorld.test.js
+++ src/HelloWorld.test.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import rendereer from 'react-test-renderer';
+import renderer from 'react-test-renderer';
 
 import HelloWorld from './HelloWorld';
+
+
+describe(HelloWorld, () => {
+  const name = 'Person';
+  const mockRemoveGreeting = jest.fn();
+  const component = shallow(
+    <HelloWorld name={name} removeGreeting={mockRemoveGreeting}/>
+  )
+});
